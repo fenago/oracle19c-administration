b83fe0042deb4db260ce25a83f841e7ea2818cb2
fenago <socrates73@gmail.com>
2021-05-02 06:31:26 +0500

    added

--- labs/OPTADV_3.sql
+++
@@ -1,8 +0,0 @@
-VAR b_report CLOB
-DECLARE
-v_tname VARCHAR2(32767);
-BEGIN
-v_tname := 'my_task';
-:b_report := dbms_stats.report_advisor_task(v_tname, type => 'TEXT', section => 'ALL', level => 'ALL');
-END;
-/
--- labs/OPTADV_5.sql
+++
@@ -1,1 +0,0 @@
-SELECT f.finding_id, f.message, r.benefit_type FROM user_advisor_findings f, user_advisor_recommendations r WHERE f.finding_id = r.finding_id AND f.task_name = 'MY_TASK' AND f.execution_name = 'EXEC_31';
